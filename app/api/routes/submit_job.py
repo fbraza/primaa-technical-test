4bae7a2ad17bb3e5a49031526ca89a746d69a232
Faouzi Braza <fbraza@tutanota.com>
2025-10-08 15:08:19 +0200

    feat & tests: add post endpoint wihtout background job for processing

---
+++ app/api/routes/submit_job.py
@@ -0,0 +1,32 @@
+from uuid import uuid4
+
+from fastapi import APIRouter, Body
+from models import JobStatusResponse, JobSubmissionRequest
+from pydantic import ValidationError
+
+from app.db import job_id_store
+
+router = APIRouter()
+
+
+@router.post("/job-submission")
+def submit_job(
+    payload: dict = Body(...),
+) -> JobStatusResponse:
+    """
+    Submit processing job
+    """
+    job_id = str(uuid4())
+    job_id_store.create(job_id)
+
+    try:
+        _ = JobSubmissionRequest(**payload)
+    except ValidationError as exc:
+        job_id_store.update(job_id=job_id, status="Failed")
+        return JobStatusResponse(
+            code=404, job_id=job_id, status=job_id_store.read(job_id), error=str(exc)
+        )
+
+    # background job
+
+    return JobStatusResponse(code=200, job_id=job_id, status=job_id_store.read(job_id))
--- app/models.py
+++ app/models.py
@@ -1,9 +1,40 @@
+import re
 from typing import Literal
 
-from pydantic import BaseModel
+from pydantic import BaseModel, model_validator
+
+VALID_URL_PATTERN = r"^https:\/\/[A-Za-z0-9.-]+(?:\/[^\s]*)?$"
 
 
 class JobStatusResponse(BaseModel):
     code: int
     job_id: str | None
     status: Literal["Pending", "Running", "Completed", "Failed", "NotFound"]
+    error: str | None = None
+
+
+class JobSubmissionRequest(BaseModel):
+    image_url: str
+    callback_url: str
+    callback_token: str
+    algorithm_name: str
+
+    @model_validator(mode="after")
+    def check_urls(self):
+        if not re.match(pattern=VALID_URL_PATTERN, string=self.image_url):
+            raise ValueError(f"Invalid email for image at : {self.image_url}")
+
+        if not re.match(pattern=VALID_URL_PATTERN, string=self.callback_url):
+            raise ValueError(f"Invalid email for callback at : {self.callback_url}")
+
+        return self
+
+    @model_validator(mode="after")
+    def check_algorithm(self):
+        valid_algorithms = ["poi_detection", "roi_detection"]
+        if self.algorithm_name not in valid_algorithms:
+            raise ValueError(
+                f"'{self.algorithm_name}' is invalid : available algorithms are '{valid_algorithms}'"
+            )
+
+        return self
